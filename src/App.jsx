e25385da33cbafdc14085137c38f40cbfaba31e7
Matt-Fulbrite <Matt-Fulbrite>
2023-09-12 14:48:31 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import './index.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
     <>
       <div>
-        
+        {
+          puppies.map((pup) => {
+            return <p onClick={() => {setFeatPupId(pup.id)}} key={pup.id}>{pup.name}</p>
+          })
+        }
+        {featPupId && <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>{featuredPup.age}</li>
+            <li>{featuredPup.email}</li>
+          </ul>
+
+          </div>
+          }
       </div>
     </>
   )
